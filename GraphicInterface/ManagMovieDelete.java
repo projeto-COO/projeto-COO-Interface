5020da8245e5632088823ab37e629861b1d9203e
rodrigolaca <rodrigolacalendola@gmail.com>
2011-06-19 11:53:18 -0700

    adicionado lista para escolher o filme para deletar.. falta a interação de exclusao do sistema..

--- GraphicInterface/ManagMovieDelete.java
+++ GraphicInterface/ManagMovieDelete.java
@@ -1,10 +1,14 @@
+
 package GraphicInterface;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
@@ -12,6 +16,9 @@ import javax.swing.border.EmptyBorder;
 public class ManagMovieDelete extends JFrame {
 
 	private JPanel contentPane;
+	private JLabel lblExcluir, lblSelecione;
+	private JComboBox comboboxSelecione;
+	private JButton btnVoltar, btnExcluir;
 
 	/**
 	 * Create the frame.
@@ -24,8 +31,26 @@ public class ManagMovieDelete extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JButton btnVoltar = new JButton("VOLTAR");
-		btnVoltar.setBounds(333, 208, 91, 43);
+		lblExcluir = new JLabel("Excluír Filmes");
+		lblExcluir.setBounds(0,0,100,20);
+		contentPane.add(lblExcluir);
+		
+		lblSelecione = new JLabel("Selecione o filme abaixo:");
+		lblSelecione.setBounds(100, 50, 300, 20);
+		contentPane.add(lblSelecione);
+		
+		comboboxSelecione = new JComboBox();
+		comboboxSelecione.addItem("exemplo1");
+		comboboxSelecione.addItem("exemplo2");
+		comboboxSelecione.addItem("exemplo3");
+		comboboxSelecione.setBounds(100, 100, 300, 20);
+		contentPane.add(comboboxSelecione);
+		
+		btnExcluir = new JButton("EXCLUIR");
+		btnExcluir.setBounds(333, 208, 91, 43);
+		contentPane.add(btnExcluir);
+		btnVoltar = new JButton("VOLTAR");
+		btnVoltar.setBounds(0, 208, 91, 43);
 		contentPane.add(btnVoltar);
 		
 		//VOLTA PARA A JANELA PRINCIPAL
@@ -35,6 +60,20 @@ public class ManagMovieDelete extends JFrame {
 				MainGraphUserInterf main = new MainGraphUserInterf();
 				main.setVisible(true);
 				setVisible(false);
+			}
+		});
+		
+		btnExcluir.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+			// IMPLEMENTAR AQUI O CÓDIGO PARA EXCLUIR O FILME;
+				
+			JOptionPane alerta = new JOptionPane();
+			alerta.showMessageDialog(contentPane, "Filme excluído com sucesso!");
+				
+			ManagMovieEditInfo info = new ManagMovieEditInfo();
+			info.setVisible(true);
+			setVisible(false);
 			}
 		});
 	}
