b528418e1470d6620f0114205524d398ee45c119
rodrigolaca <rodrigolacalendola@gmail.com>
2011-06-19 12:03:02 -0700

    exibir salas em lista

--- GraphicInterface/ManagRoomDisplay.java
+++ GraphicInterface/ManagRoomDisplay.java
@@ -1,19 +1,20 @@
 package GraphicInterface;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
+import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 
 public class ManagRoomDisplay extends JFrame {
 
 	private JPanel contentPane;
-
+	private JLabel lblExibir;
+	private JList list;
+	
 	/**
 	 * Create the frame.
 	 */
@@ -24,6 +25,23 @@ public class ManagRoomDisplay extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
+		
+		
+		lblExibir = new JLabel("Exibindo as salas cadastrados");
+		lblExibir.setBounds(0,0,250,20);
+		contentPane.add(lblExibir);
+		
+		DefaultListModel filmes = new DefaultListModel();  
+		list = new JList(filmes);
+		// AQUI SÃO AS SALAS CADASTRADOS
+		filmes.addElement("Sala 1 - 98 lugares");
+		filmes.addElement("Sala 2 - 34 lugares");
+		filmes.addElement("Sala 3 - 300 lugares");
+		filmes.addElement("Sala 4 - 50 lugares");
+		JScrollPane listScroller = new JScrollPane(list);
+		listScroller.setPreferredSize(new Dimension(400, 170));
+		listScroller.setBounds(0,30,400,170);
+		contentPane.add(listScroller);
 		
 		JButton btnVoltar = new JButton("VOLTAR");
 		btnVoltar.setBounds(333, 208, 91, 43);
