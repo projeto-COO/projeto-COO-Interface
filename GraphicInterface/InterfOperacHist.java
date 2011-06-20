dbb0cc1a667f9ef323aaf71bc59ab2cb446cd9c8
Thiago <thi_agomes@hotmail.com>
2011-06-20 14:54:33 -0300

    mais uma vez...

--- GraphicInterface/InterfOperacHist.java
+++ GraphicInterface/InterfOperacHist.java
@@ -7,14 +7,17 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.ButtonGroup;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JCheckBox;
+import javax.swing.JRadioButton;
 
 @SuppressWarnings("serial")
 public class InterfOperacHist extends JFrame {
 
 	private JPanel contentPane;
+	private ButtonGroup group;
 
 	/**
 	 * Create the frame.
@@ -33,16 +36,39 @@ public class InterfOperacHist extends JFrame {
 		contentPane.add(btnVoltar);
 		
 		JCheckBox chckbxFilme = new JCheckBox("FILME");
-		chckbxFilme.setBounds(6, 7, 55, 23);
+		chckbxFilme.setBounds(6, 7, 73, 23);
 		contentPane.add(chckbxFilme);
 		
 		JCheckBox chckbxSessao = new JCheckBox("SESSAO");
-		chckbxSessao.setBounds(63, 7, 65, 23);
+		chckbxSessao.setBounds(91, 7, 77, 23);
 		contentPane.add(chckbxSessao);
 		
 		JCheckBox chckbxSala = new JCheckBox("SALA");
-		chckbxSala.setBounds(130, 7, 51, 23);
+		chckbxSala.setBounds(188, 7, 62, 23);
 		contentPane.add(chckbxSala);
+		
+		JCheckBox chckbxCriacao = new JCheckBox("CRIACAO");
+		chckbxCriacao.setBounds(6, 33, 83, 23);
+		contentPane.add(chckbxCriacao);
+		
+		JCheckBox chckbxExclusao = new JCheckBox("EXCLUSAO");
+		chckbxExclusao.setBounds(91, 33, 91, 23);
+		contentPane.add(chckbxExclusao);
+		
+		JCheckBox chckbxModificacao = new JCheckBox("MODIFICACAO");
+		chckbxModificacao.setBounds(188, 33, 99, 23);
+		contentPane.add(chckbxModificacao);
+		
+		String[] lista = {"DATA", "SITIACAO", "TIPO"};
+		int dist = 26;
+		for (String radioB : lista){
+			JRadioButton b = new JRadioButton(radioB);
+			b.setActionCommand(radioB); // ide salvo em string
+			b.setBounds(315, 6, 109, 23);
+			group.add(b);
+			contentPane.add(b);
+			dist = dist + 26;
+		}
 
 		// VOLTA PARA A JANELA PRINCIPAL
 
--- GraphicInterface/MainGraphUserInterf.java
+++ GraphicInterface/MainGraphUserInterf.java
@@ -24,7 +24,7 @@ public class MainGraphUserInterf extends JFrame {
 	public MainGraphUserInterf() {
 		//CONSTRUTOR DO PAINEL!!!
 		setIconImage(Toolkit.getDefaultToolkit().getImage(MainGraphUserInterf.class.getResource("/com/sun/java/swing/plaf/windows/icons/Computer.gif")));
-		setTitle("MAIN MENU");
+		setTitle("MENU PRINCIPAL");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		setLocationRelativeTo(null);
